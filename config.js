8186f3a3a973467970a4041f18f5dd1b3ee31f9e
cyberm0m0 <91669894+cyberm0m0@users.noreply.github.com>
2022-12-19 21:39:34 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Secret",
+    introTitle: 'Angel',
+    introDesc: `HAHAHAHAHAHHAHAHDHFKADHFKAHDSFHAJDSLFHLHGLKSDJAFHLKSDAHFLASD`,
+    btnIntro: '^^HiHi^^',
+    title: 'QUESTION!',
+    desc: "Do you want to know frank's secret?",
+    btnYes: 'ERPMEYS',
+    btnNo: 'NOPE',
+    question: 'SINO BA KASI CRUSH NI ANGEL?',
+    btnReply: 'Submit',
+    reply: 'kapag hindi si Frank yan ediwow TT',
+    mess: 'Ikaw nga kasiiiiiiii',
+    messDesc: 'I LIKE YOU SINCE THE VERY FIRST DAY HAHSHAHA',
+    btnAccept: 'AYOS KASI',
+
+}
